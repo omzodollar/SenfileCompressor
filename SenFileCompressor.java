5e70b4dc3ca366519b43dcfaea89352a503aa580
Oumar Niang <omaryande@gmail.com>
2018-02-12 18:09:48 +0000

    first commit

---
+++ SenFileCompressor.java
@@ -0,0 +1,112 @@
+import archivage.Archivage;
+import desarchivage.Desarchivage;
+import java.lang.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class SenFileCompressor{
+  
+    public static void main(String[] args){
+
+        CharSequence cs1= "-c";
+        CharSequence cs2= "-d";
+        CharSequence cs3= "-h";
+        CharSequence cs4= "-r";
+        CharSequence cs5= "-v";
+
+        if(args.length < 1){
+
+            System.out.println("Veuillez choisir une option svp");
+        }
+        
+        else if(args[0].equals("-h"))
+        {  
+
+            for(int i=0; i<args.length; i++)
+
+            if (args[i].contains(cs1)==true || args[i].contains(cs2)==true){
+                System.out.println("Attention les options -c -d et -h sont mutuellement exclusives!!!");
+                return;
+            }
+        
+                
+            System.out.println("\n\t\t *****************************************  Aide du programme!!!  *****************************************");
+            System.out.println("\n \t Cette partie  du programme est accessible en tapant  la commande suivante: ");
+            System.out.println("\n \t java SenFileCompressor -h");
+            System.out.println("\n\t\t *******************************  Archivage de fichiers!!!  *******************************");
+            System.out.println("\n \t Ce programme permet l'archivage des fichiers c'est a dire l'utilisation d'un seul fichier pour stocker\n \t plusieurs fichiers et de pouvoir aussi a partir du fichier, regroupant le tout recuperer les fichiers d'origine");
+            System.out.println("\n\t\t ******************************* Compression de fichiers!!!  *******************************");
+            System.out.println("\n \t Il permet aussi de compresser des donnees qui consiste a diminuer l'espace occupe sur le support numerique sans perte de qualite ");
+            System.out.println("\n \t Ainsi ce module du programme est acessible via la commande:  ");
+            System.out.println("\n \t java SenFileCompressor -c <liste fichier a compresser> ");
+            System.out.println("\n \t Cette commande donnera en sortie un fichier d'extension <<.sfc>> qui regroupe sous forme compressée les \n \t differents fichiers fournis en paramètre. ");
+            System.out.println("\n \t Sur cette commande il est possible d'ajouter une option -r qui permet de specifier le chemin absolu ou relatif vers le repertoire");
+            System.out.println("\n \t ou seront stockes les fichiers produits par le programme  ou bien l'option -f si le chemin n'existe pas auquel cas, le chemin \n \t sera automatiquement créé");
+            System.out.println("\n \t Par exemple: Java SenFileCompressor –c fic1 fic2 fic3... [–r cheminVersRepertoire [–f] ] ");
+            System.out.println("\n \t Il est possible aussi de pouvoir compresser vos donnees afin de reduire la taille du fichier d'archive en faisant de sorte que \n \t la taille du fichier regroupant le tout soit la plus petite possible");
+            System.out.println("\n\t\t ****************************** Decompression de fichiers!!!  *******************************");
+            System.out.println("\n \t Sur ce programme il est possible de decompresser un fichier existant en saisissant la commande suivante ");
+            System.out.println("\n \t java SenFileCompressor -d <fichierADecompresser.sfc> ");
+            System.out.println("\n \t Cette commande devra fournir en sortie l'integralite des fichiers contenus dans l’archive donné en paramètre ");
+            System.out.println("\n \t Sur cette commande il est possible aussi  d'ajouter une option -r qui permet de specifier le chemin absolu ou relatif \n \t vers le repertoire");
+            System.out.println("\n \t ou seront stockes les fichiers produits par le programme  ou bien l'option -f si le chemin n'existe pas auquel cas, le chemin \n \t sera automatiquement créé");
+            System.out.println("\n \t Par exemple: Java SenFileCompressor –d fichier.sfc [–r cheminVersRepertoire [–f] ] ");
+            System.out.println("\n\t\t ****************************** Verbosite du programme!!!  *******************************");
+            System.out.println("\n \t L'option -v concerne la verbosite du programme qui donnera les details de l'execution du programme comme par exemple ");
+            System.out.println("\n \t Java SenFileCompressor –c fic1 fic2 fic3... [–r cheminVersRepertoire [–f] ] [–v]");
+            System.out.println("\n \t –d fichier.sfc [–r cheminVersRepertoire [–f] ] [–v]");
+            System.out.println("\n  ");        
+            
+        }
+
+        else  if(args[0].equals("-c"))
+        {
+
+            for(int i=0; i<args.length; i++)
+            if (args[i].contains(cs2)==true || args[i].contains(cs3)==true){
+                System.out.println("Attention les options -c -d et -h sont mutuellement exclusives!!!");
+                return;
+            }
+
+            for(int i=0; i<args.length; i++)
+
+            if (args[i].contains(cs4)==true || args[i].contains(cs5)==true) {
+                System.out.println("Compression de fichiers en specifiant le repertoire du fichier a compresser plus la verbosite si possible");
+                return;
+            }
+
+            
+            Archivage archive=new Archivage();
+            String dirName = args[1];
+            File dir = new File(dirName);
+            archive.packDirectory(dir);            
+        }
+
+        else if(args[0].equals("-d"))
+        {
+             for(int i=0; i<args.length; i++)
+
+                if (args[i].contains(cs1)==true || args[i].contains(cs3)==true){
+                    System.out.println("Attention les options -c -d et -h sont mutuellement exclusives!!!");
+                    return;
+                }
+           
+                System.out.println("Integralite des fichiers contenus dans l'archive!!!");
+                Desarchivage desarchive=new Desarchivage();
+                String dirName =args[1] ;
+                File dir = new File(dirName);
+                desarchive.unPack(dir);         
+   
+
+
+        }          
+        
+    }
+}
---
+++ desarchivage/Desarchivage.java
@@ -0,0 +1,102 @@
+package desarchivage;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Desarchivage {
+	/** Writer en binaire */
+	DataOutputStream dos ;
+	
+	/** Reader en binaire */
+	DataInputStream dis;
+	
+	public void readFile(File input, File output) throws IOException
+	{
+		int fileCount = dis.readInt();
+		System.out.println("L'archive contient "+  fileCount + " fichiers ");
+		for(int i = 0 ; i < fileCount ; i++)
+		{
+			String fileName = dis.readUTF();
+			long bytes = dis.readLong();
+			System.out.println(" Unpacking : " + fileName +" - " + bytes/1024 + " kb ...");
+			
+			//On cr�e le fichier
+			String filePath = output.getAbsolutePath()+"\\"+fileName;
+			File outFile = new File(filePath);
+			if(!outFile.exists())
+			{
+				outFile.createNewFile();
+			}
+
+			//on initialise le buffer d'ecriture dans le fichier out
+			dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(outFile)));
+			
+			
+			//ecriture du fichier
+			final byte[] buffer = new byte[(int)bytes];
+			int dataCount;
+			dataCount = dis.read(buffer,0,(int)bytes);
+			dos.write(buffer, 0, dataCount);
+			
+			
+			
+			//On ferme le flux d'ecriture
+			dos.flush();
+			dos.close();
+		}
+	}
+
+
+	public void unPack(File directory) {
+		//Commencer le packaging de fichier 
+		if(directory == null)
+		{
+			System.out.println("Dossier non choisi ! ");
+			return;
+		}
+		
+	
+		//On cr�e un nouveau fichier de sortie
+				File out = new File("desarchiver");
+				
+				if(!out.exists())
+				{
+					out.mkdir();
+		}
+		//On initialise le flux de sortie pour ecrire dans notre fichier de compression
+		try {
+			dis = new DataInputStream(new BufferedInputStream(new FileInputStream(directory)));
+		} catch (FileNotFoundException e1) {
+			System.out.println("Impossible de lire dans le fichier source ! ");
+			return;
+		}
+		
+		//Si on peut ecrire dans le fichier, alors on d�marre le packaging 
+		try {
+			readFile(directory,out);
+		     System.out.println("desarchivage complet : ");
+
+		} catch (IOException e2) {
+			System.out.println("Erreur lors de la lecture du fichier");
+		}
+		
+		// On ferme les flux proprement
+		try {
+			dis.close();
+		} catch (IOException e1) {
+			System.out.println("Erreur lors de la fermeture du flux d'ecriture ! ");
+			e1.printStackTrace();
+		}
+
+	}
+	 
+	
+}
+
+
